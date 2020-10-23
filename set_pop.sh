878035b7d39dbdabfaf26ccb5c0ebb36ff2abb6a
mrmurilo75 <mrmurilo75@gmail.com>
2020-10-23 16:41:03 +0100

    fix -y missing in set_pop.sh brave and full-upgrade

--- set_pop.sh
+++ set_pop.sh
@@ -34,7 +34,7 @@ echo "Installing default-JDK"
 sudo apt-get install -y default-jdk
 echo "DONE"
 echo "Installing Brave Browser"
-sudo apt-get install apt-transport-https curl
+sudo apt-get install -y apt-transport-https curl
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
@@ -61,6 +61,6 @@ ls
 echo "yyyy" | sudo gdebi *.deb
 echo "DONE"
 
-sudo apt update && sudo apt full-upgrade
+sudo apt update && sudo apt full-upgrade -y
 sudo apt autoremove -y
 
