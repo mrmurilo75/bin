34014a58cc2e034d2832af9e959a13ee7a95fa0f
mrmurilo75 <mrmurilo75@gmail.com>
2020-11-30 18:53:18 +0000

    @setups/apt-basics : add lm-sensors

--- setups/apt-basics.sh
+++ setups/apt-basics.sh
@@ -9,5 +9,7 @@ echo -e "\tDONE"
 echo -e "\n\tInstalling git"
 sudo apt-get install -y git
 echo -e "\tDONE"
+echo -e "\n\tInstalling lm-sensors"
+sudo apt-get install -y lm-sensors
+echo -e "\tDONE"
 ./install-brave-browser.sh
-
