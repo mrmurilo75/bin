76c64a9d2ae76109d7bccb95f3333166c2eafe25
Murilo Rosa <mrmurilo75@gmail.com>
2021-04-21 16:55:41 +0000

    @setups/install-dev-basics : install gdebi if not already

--- setups/install-dev-basics.sh
+++ setups/install-dev-basics.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 
+if which gdebi
+then
+	:
+else 
+	sudo apt install -y gdebi
+fi
+
 if which atom
 then
 	:
 else
-	if which gdebi
-	then
-		mkdir $HOME/.deb_packs && cd $HOME/.deb_packs
-		wget "https://atom.io/download/deb"
-		echo -e "\n\tInstalling atom as 'deb'"
-		echo "yy" | sudo gdebi *deb
-		rm -r $HOME/deb_packs
-		echo "DONE"
-	else
-		echo -e "\n\tFailed to install atom because of dependent on gdebi\nPlease install gdebi (with 'sudo apt install -y gdebi' or './apt-extras.sh') and try again"
-	fi
+	mkdir $HOME/.deb_packs && cd $HOME/.deb_packs
+	wget "https://atom.io/download/deb"
+	echo -e "\n\tInstalling atom as 'deb'"
+	echo "yy" | sudo gdebi *deb
+	rm -r $HOME/deb_packs
+	echo "DONE"
 fi
 echo -e "\n\tRunning install-github-cli.sh ..."
 ./install-github-cli.sh
