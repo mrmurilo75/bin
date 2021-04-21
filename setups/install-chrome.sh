f13cbf630c262b7c9dd37fcab333bbbf82efa00d
Murilo Rosa <mrmurilo75@gmail.com>
2021-04-21 16:58:29 +0000

    @setups/install-chrome : fix not cd-ing bug

--- setups/install-chrome.sh
+++ setups/install-chrome.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mkdir $HOME/.deb_packs && cd $HOME/.deb_packs  
-wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" 
+mkdir $HOME/.deb_packs
+cd $HOME/.deb_packs && wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 echo -e "\n\tInstalling google-chrome-stable_current_amd64.deb:" 
 ls 
-echo "yy" | sudo gdebi google-chrome-stable_current_amd64.deb 
+echo "y" | sudo gdebi google-chrome-stable_current_amd64.deb 
 rm -r $HOME/.deb_packs
