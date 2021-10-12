9937792f8d565ca490632e9e380712567e4a5c29
Murilo Rosa <mrmurilo75@gmail.com>
2021-10-12 11:06:31 +0100

    @setups : small fix in install-pip  and install-py3

--- setups/install-pip.sh
+++ setups/install-pip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install python3 python3-venv python3-pip
+sudo apt install -y python3 python3-venv python3-pip
 
 echo -e "\nPATH=$PATH:~/.local/bin\nexport PATH\n" | cat >> ~/.bashrc
--- setups/install-py3.sh
+++ setups/install-py3.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ./install-pip.sh
-echo -e "\nalias py3=\"python3\"\n" | cat >> .bashrc
+echo -e "\nalias py3=\"python3\"\n" | cat >> ~/.bashrc
+
 
