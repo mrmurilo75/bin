022b432653588ee0db625b09aa34814e88259119
Murilo Rosa <mrmurilo75@gmail.com>
2022-02-17 17:43:25 +0000

    update dev basics

--- setups/install-dev-basics.sh
+++ setups/install-dev-basics.sh
@@ -7,22 +7,7 @@ then
 else 
 	sudo apt install -y gdebi
 fi
-
-if which atom
-then
-	:
-else
-	mkdir $HOME/.deb_packs
-	cd $HOME/.deb_packs
-	wget 'https://atom.io/download/deb' 'https://discord.com/api/download?platform=linux&format=deb'
-	echo -e "\n\tInstalling all deb packs (atom, discord):"
-	echo "yy" | sudo gdebi *deb
-	rm -r $HOME/deb_packs
-	echo "DONE"
-fi
-echo -e "\n\tRunning install-github-cli.sh ..."
-./install-github-cli.sh
-echo -e "\tDONE"
+./install-discord.sh
 echo -e "\n\tInstalling default-JDK"
 sudo apt-get install -y openjdk-13-jdk
 echo -e "\tDONE"
