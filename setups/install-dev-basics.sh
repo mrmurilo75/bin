37f900126b3db56afd7a43834d5e9a1a82d94d7d
Murilo Rosa <mrmurilo75@gmail.com>
2022-02-17 18:29:29 +0000

    update dev basics

--- setups/install-dev-basics.sh
+++ setups/install-dev-basics.sh
@@ -9,6 +9,12 @@ else
 fi
 ./install-discord.sh
 ./install-py3.sh
+echo -e "\n\tInstalling nodejs"
+sudo apt-get install -y nodejs
+echo -e "\tDONE"
+echo -e "\n\tInstalling npm"
+sudo apt-get install -y npm
+echo -e "\tDONE"
 echo -e "\n\tInstalling default-JDK"
 sudo apt-get install -y openjdk-13-jdk
 echo -e "\tDONE"
