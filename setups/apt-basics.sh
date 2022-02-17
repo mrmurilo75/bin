cbf3d7a575c11b0f4eb4478753ff2cb09a3bef96
Murilo Rosa <murilorosa@pop-os.localdomain>
2022-02-17 17:40:46 +0000

    update apt basics

--- setups/apt-basics.sh
+++ setups/apt-basics.sh
@@ -9,6 +9,9 @@ echo -e "\tDONE"
 echo -e "\n\tInstalling git"
 sudo apt-get install -y git
 echo -e "\tDONE"
+echo -e "\n\tInstalling xclip"
+sudo apt-get install -y xclip
+echo -e "\tDONE"
 echo -e "\n\tInstalling lm-sensors"
 sudo apt-get install -y lm-sensors
 echo -e "\tDONE"
