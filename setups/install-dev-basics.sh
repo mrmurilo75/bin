61aba848d3348ba313731e5dd4570427aa6b3a4e
Murilo Rosa <mrmurilo75@gmail.com>
2022-02-17 17:52:52 +0000

    update dev basics

--- setups/install-dev-basics.sh
+++ setups/install-dev-basics.sh
@@ -7,9 +7,11 @@ then
 else 
 	sudo apt install -y gdebi
 fi
-./install-py3.sh
 ./install-discord.sh
+./install-py3.sh
 echo -e "\n\tInstalling default-JDK"
 sudo apt-get install -y openjdk-13-jdk
 echo -e "\tDONE"
+
+brave-browser 'https://www.jetbrains.com/pycharm/download/#section=linux'
 
