b014dbef796548099e9e90dac4f85ce4a2fc9597
Murilo Rosa <mrmurilo75@gmail.com>
2022-03-03 11:54:46 +0000

    set swapescape option in /etc/default

--- setups/set-bashrc.sh
+++ setups/set-bashrc.sh
@@ -2,6 +2,6 @@
 
 echo -e "\n\tOn .bashrc :\n\t\tAdd ~/bin to PATH variable\n"
 echo -e "\nPATH=$PATH:$HOME/bin \nexport PATH\n" | cat >> $HOME/.bashrc
-echo -e "\n\t\tChange CapsLock to ESC\n"
-echo -e "\nsetxkbmap -option caps:swapescape\n" | cat >> $HOME/.bashrc
-echo -e "\tDone"
+
+echo -e "\nXKBOPTIONS=\"caps:swapescape\"\n" | cat >> /etc/default/keyboard`
+
