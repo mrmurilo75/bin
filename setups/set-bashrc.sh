759805513521d0d04675640dae1373dfbd4d4257
Murilo Rosa <mrmurilo75@gmail.com>
2022-03-12 18:21:22 +0000

    fix xkbmap casp:swapescape

--- setups/set-bashrc.sh
+++ setups/set-bashrc.sh
@@ -3,5 +3,6 @@
 echo -e "\n\tOn .bashrc :\n\t\tAdd ~/bin to PATH variable\n"
 echo -e "\nPATH=$PATH:$HOME/bin \nexport PATH\n" | cat >> $HOME/.bashrc
 
-echo -e "\nXKBOPTIONS=\"caps:swapescape\"\n" | cat >> /etc/default/keyboard
+touch ~/.xinitrc
+echo -e "\nsetxkbmap -option caps:swapescape\n" | cat >> ~/.xinitrc
 
