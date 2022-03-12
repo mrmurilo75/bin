cd99a5d554e6823b8e4bb7457ac058b84637dcd9
Murilo Rosa <mrmurilo75@gmail.com>
2022-03-12 22:11:07 +0000

    add xournal appimage

--- setups/appimage-basics.sh
+++ setups/appimage-basics.sh
@@ -11,7 +11,7 @@ if ls $HOME/Programs | grep VidCutter-6.0.0-x64.AppImage
 then
 	:
 else
-	cd $HOME/Programs && wget 'https://github.com/ozmartian/vidcutter/releases/download/6.0.0/VidCutter-6.0.0-x64.AppImage'
-	chmod 744 VidCutter-6.0.0-x64.AppImage
+	cd $HOME/Programs && wget 'https://github.com/ozmartian/vidcutter/releases/download/6.0.0/VidCutter-6.0.0-x64.AppImage' 'https://github.com/xournalpp/xournalpp/releases/download/v1.1.1/xournalpp-1.1.1-x86_64.AppImage'
+	chmod 744 *.AppImage
 fi
 
