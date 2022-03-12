a540076950720e1d18cc5ec5723c4f727c826675
Murilo Rosa <mrmurilo75@gmail.com>
2022-03-12 18:18:57 +0000

    fix mistype

--- setups/set-bashrc.sh
+++ setups/set-bashrc.sh
@@ -3,5 +3,5 @@
 echo -e "\n\tOn .bashrc :\n\t\tAdd ~/bin to PATH variable\n"
 echo -e "\nPATH=$PATH:$HOME/bin \nexport PATH\n" | cat >> $HOME/.bashrc
 
-echo -e "\nXKBOPTIONS=\"caps:swapescape\"\n" | cat >> /etc/default/keyboard`
+echo -e "\nXKBOPTIONS=\"caps:swapescape\"\n" | cat >> /etc/default/keyboard
 
