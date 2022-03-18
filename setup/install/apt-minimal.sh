cd55ff017ea92c2aa15c4849d7ee5a64a6f2fe69
Murilo Rosa <mrmurilo75@gmail.com>
2022-03-18 22:15:39 +0000

    setup - add apt-minimal

---
+++ setup/install/apt-minimal.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+echo -e "\nWould you like to install apt-essencials now (this may take a while)? [y/N]  "
+
+read input
+
+if [[ $input =~ ('y'|'Y'|'yes'|'Yes'|'YES') ]]; then
+	./apt-essencials.sh
+else
+	echo -e 'Essencials from apt have NOT been installed!\nPlease run:\n\t setup/install/apt-essencials.sh\nat a convenient moment'
+fi
+
+echo -e "\n\tInstalling neovim git lm-sensors psensors --------------\n"
+sudo apt-get install -y neovim git lm-sensors psensors
+echo -e "\n\tDONE ---------------------------------------------------\n"
+
