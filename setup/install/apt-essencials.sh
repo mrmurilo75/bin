15c397e3d0f72b17a9549bd4b3002b0e374ca53e
Murilo Rosa <mrmurilo75@gmail.com>
2022-03-18 22:12:00 +0000

    setup - apt-essencials - add y option and compact

--- setup/install/apt-essencials.sh
+++ setup/install/apt-essencials.sh
@@ -3,15 +3,9 @@
 
 sudo apt update 
 echo -e "\n\tInstalling linux-kernel"
-sudo apt-get install linux-kernel && sudo update-initramfs && sudo apt update && sudo apt full-upgrade
+sudo apt-get install -y linux-kernel && sudo update-initramfs && sudo apt update && sudo apt full-upgrade -y
 echo -e "\tDONE\n"
 echo -e "\n\tInstalling build-essential"
-sudo apt-get install -y build-essential
-echo -e "\tDONE\n"
-echo -e "\n\tInstalling gcc-multilib"
-sudo apt-get install -y gcc-multilib
-echo -e "\tDONE\n"
-echo -e "\n\tInstalling util-linux"
-sudo apt-get install -y util-linux
+sudo apt-get install -y build-essential gcc-multilib util-linux
 echo -e "\tDONE\n"
 
