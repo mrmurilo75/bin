5fde84a881dcd7f3253874eab4b8c78fe39f68f0
Murilo Rosa <mrmurilo75@gmail.com>
2022-03-18 21:05:20 +0000

    clean up 5o_sem

--- get-comp-x.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd ~/Desktop/git/ && git clone git@github.com:compiladores-cc3001/aula-laboratorial-$1-mrmurilo75.git
-
--- set_starts_5oSem.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo -e "\nalias git-status=\"git fetch;git status;echo;git log -1\" start_pdf=\"cd ~/Desktop/PDFSignature; source bin/activate; cd assinatura-digital-pdf/assinatura_digital_pdf/; git-status\" start_cp=\"cd ~/Desktop/git/competitive_programming_2122/; git-status\"\n" | cat >> ~/.bashrc
-
