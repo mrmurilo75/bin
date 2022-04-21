94def8cb04de49b51030a0325d4bc702670d5632
mrmurilo75 <mrmurilo75@pop-os.localdomain>
2022-04-21 22:22:57 +0100

    fix pycharm installer to download tgz

--- setup/install/installer/devs/pycharm.sh
+++ setup/install/installer/devs/pycharm.sh
@@ -2,6 +2,5 @@
 
 ./config/installer-config.sh
 
-cd $HOME/deb_packs && wget "https://github.com/xournalpp/xournalpp/releases/download/v1.1.1/xournalpp-1.1.1-Debian-bullseye-x86_64.deb"
-echo "y" | sudo gdebi google-chrome-stable_current_amd64.deb 
+cd $HOME/ && wget "https://download.jetbrains.com/python/pycharm-professional-2022.1.tar.gz"
 
